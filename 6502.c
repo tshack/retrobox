0edbc7102174f3775cc4654a8ed1aa3e46d3362b
tshack <tshack@141d9388-71d3-473b-b169-718772bfc369>
2010-11-08 22:10:59 +0000

    Update to conform to GPLv3

--- 6502.c
+++ 6502.c
@@ -1,9 +1,21 @@
-/************************************************************
- *    Filename: 6502.c                                      *
- *      Author: James A. Shackleford (retro.box@tshack.net) *
- *        Date: Sept 17th, 2010                             *
- *     License: See COPYRIGHT.TXT                           *
- ************************************************************/
+/*  This file is part of retrobox
+    Copyright (C) 2010  Jamse A. Shackleford
+
+    retrobox is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// file created: Sept 17th, 2010
 
 // TODO:
 //  1) Get 06-abs_xy.nes to pass:
--- memory.h
+++ memory.h
@@ -1,9 +1,21 @@
-/************************************************************
- *    Filename: memory.h                                    *
- *      Author: James A. Shackleford (retro.box@tshack.net) *
- *        Date: Sept 30th, 2010                             *
- *     License: See COPYRIGHT.TXT                           *
- ************************************************************/
+/*  This file is part of retrobox
+    Copyright (C) 2010  Jamse A. Shackleford
+
+    retrobox is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// file created: Sept 30th, 2010
 #ifndef _memory_h_
 #define _memory_h_
 
